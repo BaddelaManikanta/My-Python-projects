9af5403e191a4ab8a73034649d934bbb35449636
BaddelaManikanta <baddelamanikanta@gmail.com>
2024-02-22 14:45:59 +0530

    changes

--- project2.py
+++ project2.py
@@ -1,28 +1,28 @@
-with open("story.txt", "r") as f:
-    story = f.read()
+# with open("story.txt", "r") as f:
+#     story = f.read()
 
-words = set()
-start_of_word = -1
+# words = set()
+# start_of_word = -1
 
-target_start = "<"
-target_end = ">"
+# target_start = "<"
+# target_end = ">"
 
-for i, char in enumerate(story):
-    if char == target_start:
-        start_of_word = i
+# for i, char in enumerate(story):
+#     if char == target_start:
+#         start_of_word = i
 
-    if char == target_end and start_of_word != -1:
-        word = story[start_of_word: i + 1]
-        words.add(word)
-        start_of_word = -1
+#     if char == target_end and start_of_word != -1:
+#         word = story[start_of_word: i + 1]
+#         words.add(word)
+#         start_of_word = -1
 
-answers = {}
+# answers = {}
 
-for word in words:
-    answer = input("Enter a word for " + word + ": ")
-    answers[word] = answer
+# for word in words:
+#     answer = input("Enter a word for " + word + ": ")
+#     answers[word] = answer
 
-for word in words:
-    story = story.replace(word, answers[word])
+# for word in words:
+#     story = story.replace(word, answers[word])
 
-print(story)
+# print(story)
